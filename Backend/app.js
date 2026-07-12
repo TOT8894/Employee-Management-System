0a0e44f41f8ceea8c0a5890194ee286ee15debbc
shanbel <shanbeldires7@gmail.com>
2026-07-12 15:02:09 +0300

    adding auth route employee route and use mongodb connection function

--- Backend/app.js
+++ Backend/app.js
@@ -1,12 +1,19 @@
 import express from "express";
+import mongodb from "./config/mongodb.js";
 import dotenv from "dotenv";
-import cors from "cors";
-
+import employeesRoute from "./routes/employeesRoute.js";
+import authRoute from "./routes/authRoute.js";
+import adminRoute from "./routes/adminRoute.js";
+import cookieParser from "cookie-parser";
+import { errorHandler } from "./errorHandler/errorHandler.js";
 dotenv.config();
-const app=express();
-app.use(cors());
+mongodb();
+const app = express();
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use("/api/v1/admin",adminRoutes);
-app.use(errorHandler)
+app.use(cookieParser());
+app.use("/api/v1/employees", employeesRoute);
+app.use("/api/v1/admin", adminRoute);
+app.use("/api/v1/auth", authRoute);
+app.use(errorHandler);
+
 export default app;
