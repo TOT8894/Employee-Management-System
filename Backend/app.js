72bb14e77788ef50d51b8b02eb55a6173e80cf61
shanbel <shanbeldires7@gmail.com>
2026-07-12 14:52:39 +0300

    initializing the app component

---
+++ Backend/app.js
@@ -0,0 +1,12 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+const app=express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/api/v1/admin",adminRoutes);
+app.use(errorHandler)
+export default app;
