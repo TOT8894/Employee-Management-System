3b53d82403620b2a86f80469242575d030bd43ca
shanbel <shanbeldires7@gmail.com>
2026-07-13 10:36:43 +0300

    import and use header component for rendering

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
-
+import {Header} from "./components/HeaderComponent/Header.jsx"
 import { Profile } from './components/ProfileComponent/Profile.jsx'
 
 function App() {
   return (
     <>
+    <Header/>
      <Profile/>
+    
     </>
   )
 }
